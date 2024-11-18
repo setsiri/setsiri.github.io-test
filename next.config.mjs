379dc53496896a97194d548403cc90e58ae1fe98
setsiri matewin <setsiri_matewin@cmu.ac.th>
2024-11-18 11:51:07 +0700

    test nextjs push

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  output: 'export', // เปิดใช้ Static Export
+  basePath: isProd ? '/setsiri.github.io' : '',
+  assetPrefix: isProd ? '/setsiri.github.io/' : '',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
